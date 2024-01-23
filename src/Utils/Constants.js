a8494ed25b0e1c88bc6378e570b23b10b75819ae
shruti garg <hellocoders8213@gmail.com>
2024-01-23 22:03:14 +0530

    swiggy menu api fixed

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -4,8 +4,11 @@ export const imgCdnUrl =
 export const swiggyApiUrl =
 	"https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.9844618&lng=77.7064137&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
+// export const swiggyMenuApiURL =
+// 	"https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=28.9844618&lng=77.7064137&&submitAction=ENTER&restaurantId=";
+
 export const swiggyMenuApiURL =
-	"https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=28.9844618&lng=77.7064137&&submitAction=ENTER&restaurantId=";
+	"https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.9844618&lng=77.7064137&restaurantId=";
 
 export const img_Menu_Food =
 	"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
